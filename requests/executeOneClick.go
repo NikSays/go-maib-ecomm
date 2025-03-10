fdd47996dcb3737d1cf1abe264011c443f9a581f
ns <niksaysit@gmail.com>
2025-03-10 15:15:58 +0200

    Add oneClick transaction support

---
+++ requests/executeOneClick.go
@@ -0,0 +1,71 @@
+package requests
+
+import (
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+
+	"github.com/NikSays/go-maib-ecomm/internal/validators"
+	"github.com/NikSays/go-maib-ecomm/types"
+)
+
+const executeOneClickCommand = "f"
+
+// ExecuteOneClick executes a oneClick transaction (-f) after it was created with
+// [RegisterOneClick] (-z/-p with oneclick=Y). It should be finalized with
+// [TransactionStatus] (-c).
+type ExecuteOneClick struct {
+	// Transaction payment amount. Positive integer with last 2 digits being the cents.
+	//
+	// Example: if Amount:199 and Currency:CurrencyUSD, $1.99 will be requested from the client's card.
+	Amount uint `url:"amount"`
+
+	// Transaction currency in ISO4217 3 digit format.
+	Currency types.Currency `url:"currency"`
+
+	// Client's IP address in quad-dotted notation, like "127.0.0.1".
+	ClientIPAddress string `url:"client_ip_addr"`
+
+	// Transaction details. Optional.
+	Description string `url:"description,omitempty"`
+
+	// Identifier of the oneClick payment.
+	BillerClientID string `url:"biller_client_id"`
+}
+
+// ExecuteOneClickResult contains the response to a ExecuteOneClick request.
+type ExecuteOneClickResult struct {
+	// ID of the executed transaction. 28 symbols in base64.
+	TransactionID string `mapstructure:"TRANSACTION_ID"`
+
+	// Transaction result status.
+	Result types.ResultEnum `mapstructure:"RESULT"`
+
+	// Transaction result code returned from Card Suite FO (3 digits).
+	ResultCode int `mapstructure:"RESULT_CODE"`
+
+	// Retrieval reference number returned from Card Suite FO.
+	RRN int `mapstructure:"RRN"`
+
+	// Approval Code returned from Card Suite FO (max 6 characters).
+	ApprovalCode string `mapstructure:"APPROVAL_CODE"`
+}
+
+func (payload ExecuteOneClick) Values() (url.Values, error) {
+	v, err := query.Values(payload)
+	if err != nil {
+		return nil, err
+	}
+	v.Set("command", executeOneClickCommand)
+	return v, nil
+}
+
+func (payload ExecuteOneClick) Validate() error {
+	return validators.Validate(
+		validators.WithAmount(payload.Amount, true),
+		validators.WithCurrency(payload.Currency),
+		validators.WithClientIPAddress(payload.ClientIPAddress),
+		validators.WithDescription(payload.Description),
+		validators.WithBillerClientID(payload.BillerClientID, true),
+	)
+}
--- requests/registerOneClick.go
+++ requests/registerOneClick.go
@@ -9,44 +9,41 @@ import (
 	"github.com/NikSays/go-maib-ecomm/types"
 )
 
-// RegisterRecurringType holds possible types for recurring transaction.
-type RegisterRecurringType int
+// RegisterOneClickType holds possible types for recurring transaction.
+type RegisterOneClickType int
 
 const (
-	// RegisterRecurringSMS is a recurring transaction type which is initialized with an SMS transaction (-z).
+	// RegisterOneClickSMS is a oneClick transaction type which is initialized with an SMS transaction (-z).
 	// The resulting transaction should be confirmed with TransactionStatus (-c).
 	//
 	// This is the default transaction type.
-	RegisterRecurringSMS RegisterRecurringType = iota
+	RegisterOneClickSMS RegisterOneClickType = iota
 
-	// RegisterRecurringDMS is a recurring transaction type which is initialized with a DMS transaction (-d).
-	// The resulting transaction should be confirmed with TransactionStatus (-c), and executed with ExecuteDMS (-t).
-	RegisterRecurringDMS
-
-	// RegisterRecurringWithoutPayment is a recurring transaction type which is initialized without a transaction (-p).
-	RegisterRecurringWithoutPayment
+	// RegisterOneClickWithoutPayment is a oneClick transaction type which is initialized without a transaction (-p).
+	RegisterOneClickWithoutPayment
 )
 
-// String converts RegisterRecurringType into the ECommerce command.
+// String converts RegisterOneClickType into the ECommerce command.
 // Returns an empty string for unknown values.
-func (t RegisterRecurringType) String() string {
+func (t RegisterOneClickType) String() string {
 	switch t {
-	case RegisterRecurringSMS:
+	case RegisterOneClickSMS:
 		return "z"
-	case RegisterRecurringDMS:
-		return "d"
-	case RegisterRecurringWithoutPayment:
+	case RegisterOneClickWithoutPayment:
 		return "p"
 	default:
 		return ""
 	}
 }
 
-// RegisterRecurring creates a new recurring transaction.
-type RegisterRecurring struct {
-	// Transaction type used for registration. Can be SMS (-z), DMS (-d), or without first payment (-p).
+// RegisterOneClick creates a new oneClick transaction. This allows the merchant
+// to save a card, like with recurring transactions. However, oneClick transactions
+// cannot be instantly executed. The client must be redirected to the client handler
+// to confirm the transaction.
+type RegisterOneClick struct {
+	// Transaction type used for registration. Can be SMS (-z), or without first payment (-p).
 	// Default is SMS.
-	TransactionType RegisterRecurringType `url:"-"`
+	TransactionType RegisterOneClickType `url:"-"`
 
 	// Transaction payment amount. Positive integer with last 2 digits being the cents.
 	// Ignored for registration without first payment.
@@ -66,29 +63,33 @@ type RegisterRecurring struct {
 	// Language in which the bank payment page will be displayed.
 	Language types.Language `url:"language"`
 
-	// Identifier of the recurring payment. If not specified,
-	// resulting TRANSACTION_ID will be used as the recurring payment ID.
+	// Identifier of the oneClick payment. If not specified,
+	// resulting TRANSACTION_ID will be used as the oneClick payment ID.
 	BillerClientID string `url:"biller_client_id"`
 
 	// Validity limit of the regular payment in the format "MMYY".
 	PerspayeeExpiry string `url:"perspayee_expiry"`
 
-	// Whether the recurring transaction with a given BillerClientID should be updated.
+	// Whether the oneClick transaction with a given BillerClientID should be updated.
 	// This way, same BillerClientID may be used when customer changes payment information.
 	OverwriteExisting bool `url:"-"`
 }
 
-// RegisterRecurringResult contains the response to a RegisterRecurring request.
-type RegisterRecurringResult struct {
+// RegisterOneClickResult contains the response to a RegisterOneClick request.
+type RegisterOneClickResult struct {
 	// ID of the created transaction. 28 symbols in base64.
 	TransactionID string `mapstructure:"TRANSACTION_ID"`
 }
 
-func (payload RegisterRecurring) Values() (url.Values, error) {
+func (payload RegisterOneClick) Values() (url.Values, error) {
 	v, err := query.Values(payload)
 	if err != nil {
 		return nil, err
 	}
+
+	// todo add param here and in recurring
+	// v.Set("ask_save_card_data", "True")
+	v.Set("oneclick", "Y")
 
 	v.Set("perspayee_gen", "1")
 	if payload.OverwriteExisting {
@@ -96,16 +97,16 @@ func (payload RegisterRecurring) Values() (url.Values, error) {
 	}
 
 	// Amount not needed for -p
-	if payload.TransactionType == RegisterRecurringWithoutPayment {
+	if payload.TransactionType == RegisterOneClickWithoutPayment {
 		v.Del("amount")
 	}
 	v.Set("command", payload.TransactionType.String())
 	return v, nil
 }
 
-func (payload RegisterRecurring) Validate() error {
+func (payload RegisterOneClick) Validate() error {
 	isAmountRequired := true
-	if payload.TransactionType == RegisterRecurringWithoutPayment {
+	if payload.TransactionType == RegisterOneClickWithoutPayment {
 		isAmountRequired = false
 	}
 	return validators.Validate(
