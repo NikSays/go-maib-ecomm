4d0ca765d109da7942323f20dc33d160a25606b2
ns <niksaysit@gmail.com>
2025-03-31 18:06:42 +0300

    Un-export Sender interface

--- requests/result.go
+++ requests/result.go
@@ -14,5 +14,5 @@ type resultTypes interface {
 // into any Result type. The generic type must be specified.
 func DecodeResponse[ResultType resultTypes](maibResponse map[string]any) (result ResultType, err error) {
 	err = mapstructure.Decode(maibResponse, &result)
-	return result, err
+	return
 }
